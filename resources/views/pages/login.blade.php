f61806478c26caefac6636569e4243fc8b4451ed
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-27 12:52:54 +0700

    login

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -4,9 +4,10 @@
 	<br>
 	<br>
 	<div class="row align-items-center">
-		<div class="col-md-5 col-sm-offset-1">
-			<h1 class="atas">Login here :</h1>
+		<div class="col-xs-6 col-md-4"></div>
+		<div class="col-xs-6 col-md-4">
 			{!! Form::open(array('route' => 'logins.store')) !!}
+			<h1 class="active">Login here :</h1>
 			<div class="imgcontainer">
 				<img src="img/avatar.png" alt="Avatar" class="avatar">
 			</div>
@@ -15,10 +16,11 @@
 
 			  {{ Form::label('password', 'Password: ') }}
 			  {{ Form::password('password', array('class' => 'form-control', 'placeholder' => 'Password')) }}
-			  {{ Form::submit('Login', array('class' => 'btn btn-success btn-md')) }}
+			  {{ Form::submit('Login', array('class' => 'tombol')) }}
 			{!! Form::close() !!}
 		</div>
-		<div class="col-md-5">
+		<div class="col-xs-6 col-md-4"></div>
+		<!-- <div class="col-md-5">
 			<h1 class="atas">Sign Up:</h1>
 			{!! Form::open(array('route' => 'logins.store')) !!}
 			<p> Please fill this form to create an account</p><hr>
@@ -33,17 +35,24 @@
 
 			  {{ Form::label('alamat', 'Alamat: ') }}
 			  {{ Form::text('alamat', null, array('class' => 'form-control', 'placeholder' => 'Alamat')) }}
-
-			  {{ Form::label('jenis_kel', 'Jenis Kelamin(L/P): ') }}
-			  {{ Form::radio('jenis_kel', 'Perempuan', true) }}
-			  {{ Form::radio('jenis_kel', 'Laki-laki') }}
-
+			  <div>
 			  {{ Form::label('tgl_lahir', 'Tanggal Lahir: ', array('style' => 'margin-left: 50px')) }}
 			  {{ Form::date('tgl_lahir', null, array('class' => 'form-contol')) }}
+			  </div>
+			  {{ Form::label('jenis_kel', 'Jenis Kelamin:') }}
+			  <br>
+			  <div class="radio-inline">
+				  <label>
+				    <input type="radio" name="jenis_kel" id="optionsRadios1" value="option1" checked>Perempuan
+				  </label>
+				</div>
+				<div class="radio-inline">
+				  <label>
+				    <input type="radio" name="jenis_kel" id="optionsRadios2" value="option2">
+				    Laki-Laki
+				  </label>
+			  </div>
 			  {{ Form::submit('Submit', array('class' => 'btn btn-success btn-md btn-block')) }}
 			{!! Form::close() !!}
-			  </div>
-			  </div>
-		</div>
+			  </div> -->
 	</div>
-</div>
