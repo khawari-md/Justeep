4a42f07297cb4685687d2d55fba90acf461c3cb2
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-28 18:22:47 +0700

    profil

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -2,45 +2,52 @@
 
 @section('content')
 	<div class="atas"></div>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-7 ">
-				<div class="panel panel-default">
-					<div class="panel-heading">
-						<h4 >User Profile</h4></div>
-						<div class="panel-body">
-							<div class="box box-info">
-								<div class="box-body">
-									<div class="col-sm-6">
-										<div  align="center"> <img class="ava_prof" src="img/avatar.png"></div>
-									</div>
-									<div class="col-sm-6">
-										<span><h4 style="color:#00b1b1;">ngambil dari database</h4></span>
-									</div>
-									<div class="clearfix"></div>
-									<hr style="margin:5px 0 5px 0;">
-									<div class="col-sm-5 col-xs-6 tital " >Name :</div><div class="col-sm-7 col-xs-6 ">ngambil dari database</div>
-									<div class="clearfix"></div>
-									<div class="col-sm-5 col-xs-6 tital " >Email :</div><div class="col-sm-7">ngambil dari database</div>
-									<div class="clearfix"></div>
-									<div class="bot-border"></div>
-									<div class="col-sm-5 col-xs-6 tital " >Alamat :</div><div class="col-sm-7">ngambil dari database</div>
-									<div class="clearfix"></div>
-									<div class="bot-border"></div>
-									<div class="col-sm-5 col-xs-6 tital " >Jenis Kelamin :</div><div class="col-sm-7">ngambil dari database</div>
-									<div class="clearfix"></div>
-									<div class="bot-border"></div>
-									<div class="col-sm-5 col-xs-6 tital " >Tanggal Lahir :</div><div class="col-sm-7">ngambil dari database</div>
-									<div class="clearfix"></div>
-									<div class="bot-border"></div>
-									<div class="col-sm-5 col-xs-6 tital " >Rateing :</div><div class="col-sm-7">ngambil dari database</div>
-								</div>
-							</div>
-						</div>
+	<div class="container" style="width: 750px;">
+		<div class="panel panel-warning">
+			<div class="panel-heading" style="text-align: center;">My Profile</div>
+			<div class="panel-body">
+				<div class="row">
+					<div class="imgcontainer">
+						<!-- ini fotonya nggak bisa diupdate -->
+						<img src="img/avatar.png" class="avatar" style="width: 30%;">
+					</div>
+					<div class="text-center">
+						<a href="#"><button class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span> Update Profile</button></a>
 					</div>
 				</div>
-			<div class="col-xs-12 col-sm-5">
-				text
+				<br>
+				<div class="col-sm-3 text-center">
+					<div style="font-size:3em; color: sky blue">
+						<i class="fas fa-gift"></i>
+					</div>
+				</div>
+				<div class="col-sm-3 text-center">
+					<i class="fas fa-paper-plane"></i>
+				</div>
+				<div class="col-sm-3 text-center">
+					tes
+				</div>
+				<div class="col-sm-3 text-center">
+					tes
+				</div>
+				<br>
+				<hr>
+				<div class="col-md-6 text-center">
+					<p><strong>Nama</strong></p>
+					<p><strong>Username</strong></p>
+					<p><strong>Email</strong></p>
+					<p><strong>Alamat</strong></p>
+					<p><strong>Jenis Kelamin</strong></p>
+					<p><strong>Tanggal Lahir</strong></p>
+				</div>
+				<div class="col-md-6 text-center">
+					<p>ambil dari database</p>
+					<p>ambil dari database</p>
+					<p>ambil dari database</p>
+					<p>ambil dari database</p>
+					<p>ambil dari database</p>
+					<p>ambil dari database</p>
+				</div>
 			</div>
 		</div>
 	</div>
