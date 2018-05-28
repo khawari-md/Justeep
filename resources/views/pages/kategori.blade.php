6b7df922c8ca03a75936cfa51dd08af0bb40d786
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-29 02:56:29 +0700

    kategori

---
+++ resources/views/pages/kategori.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.navbar')
+
+@section('content')
+<div class="atas">Category</div>
+<br>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-4">
+			<div class="panel panel-primary">
+				<div class="panel-heading text-center">
+					<h1 class="active">Nama Kategori ya</h1>
+				</div>
+				<div class="panel-body">
+					<img src="harusnya ini diambil dari db">
+				</div>
+				<div class="panel-footer">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
