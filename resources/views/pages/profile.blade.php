52b05b5545450522bdc74fedaadf4ab8f3251512
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-29 01:10:15 +0700

    profile

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -9,9 +9,16 @@
 			</div>
 			<div class="panel-body">
 				<div class="row">
-					<div class="imgcontainer">
-						<!-- ini fotonya nggak bisa diupdate -->
-						<img src="img/avatar.png" class="avatar" style="width: 30%;">
+					<div class="col-xs-3">
+						<div class="img_prof">
+							<!-- ini fotonya nggak bisa diupdate -->
+							<img src="img/avatar.png" class="avatar" style="width: 20%;">
+						</div>
+					</div>
+					<div class="col-xs-7">
+						<h2>ini ntar nama ambil dari database ya</h2>
+						<p>About Me<br>ini ntar ambil dari database setelah diupdate ya</p>
+						<p>Rate :</p>
 					</div>
 					<div class="text-center">
 						<a href="#"><button class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span> Update Profile</button></a>
@@ -52,6 +59,13 @@
 
 					<div class="col-md-6 text-center">
 						<p><strong>Email</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<p><strong>No. Telp</strong></p>
 					</div>
 					<div class="col-md-6 text-center">
 						<p>ambil dari database</p>
