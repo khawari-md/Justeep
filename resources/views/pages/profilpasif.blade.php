06eb839ccc690e03107610bfec2540716c43a917
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-29 01:16:35 +0700

    profil pasif

---
+++ resources/views/pages/profilpasif.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.navbar')
+
+@section('content')
+	<div class="atas"></div>
+	<div class="container" style="width: 70%;">
+		<div class="panel panel-primary">
+			<div class="panel-heading" style="text-align: center;">
+				<h1 class="active">My Profile</h1>
+			</div>
+			<div class="panel-body">
+				<div class="row">
+					<div class="col-xs-3">
+						<div class="img_prof">
+							<!-- ini fotonya nggak bisa diupdate -->
+							<img src="img/avatar.png" class="avatar" style="width: 20%;">
+						</div>
+					</div>
+					<div class="col-xs-7">
+						<h2>ini ntar nama ambil dari database ya</h2>
+						<p>About Me<br>ini ntar ambil dari database setelah diupdate ya</p>
+						<p>Rate :</p>
+					</div>
+				</div>
+				<br>
+				<div class="container-fluid">
+					<div class="col-md-6 text-center">
+						<div style="font-size:3em; color: sky blue">
+							<i class="fas fa-gift"></i>
+						</div>
+						<p>REQUEST</p>
+						<p><strong>ambil dari database yaaaaa</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<div style="font-size:3em; color: sky blue">
+							<i class="fas fa-paper-plane"></i>
+						</div>
+						<p>OFFER</p>
+						<p><strong>ambil dari database yaaaaa</strong></p>
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-6 text-center">
+						<p><strong>Nama</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<p><strong>Username</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<p><strong>Email</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<p><strong>No. Telp</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<p><strong>Alamat</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<p><strong>Jenis Kelamin</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<p><strong>Tanggal Lahir</strong></p>
+					</div>
+					<div class="col-md-6 text-center">
+						<p>ambil dari database</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
--- resources/views/pages/updateprof.blade.php
+++ resources/views/pages/updateprof.blade.php
@@ -7,24 +7,14 @@
 	{{ Form::label('user_nama', 'Nama : ') }}
 	{{ Form::text('user_nama', null, array('class' => 'form-control', 'placeholder' => 'Nama...')) }}
 	<br>
-	{{ Form::label('alamat', 'Alamat : ') }}
+	{{ Form::label('alamat', 'Address : ') }}
 	{{ Form::textarea('alamat', null, array('class' => 'form-control', 'placeholder' => 'Jl.xxx...')) }}
-
-	{{ Form::label('jenis_kel', 'Jenis Kelamin: ')}}
 	<br>
-	<div class="radio-inline">
-		<label>
-				<input type="radio" name="onr" id="optionsRadios1" value="Perempuan" checked>
-				Perempuan
-		</label>
-	</div>
-	<div class="radio-inline">
-		<label>
-				<input type="radio" name="onr" id="optionsRadios2" value="Laki-Laki">
-				Laki-Laki
-		</label>
-	</div>
+	{{ Form::label('no_telp', 'Telephone Number : ') }}
+	{{ Form::text('no_telp', null, array('class' => 'form-control', 'placeholder' => '0876767xxxx')) }}
 	<br>
+	{{ Form::label('aboutme', 'About Me : ') }}
+	{{ Form::textarea('aboutme', null, array('class' => 'form-control', 'placeholder' => 'Saya suka .......')) }}
 	{{ Form::submit('DONE', array('class' => 'tombol')) }}
   {!! Form::close() !!}
 </div>
