7e8f6537118b4230a32b302a14642a3ccedc340d
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-29 12:47:58 +0700

    kategori

--- resources/views/pages/kategori.blade.php
+++ resources/views/pages/kategori.blade.php
@@ -5,16 +5,43 @@
 <br>
 <div class="container">
 	<div class="row">
-		<div class="col-sm-4">
-			<div class="panel panel-primary">
-				<div class="panel-heading text-center">
-					<h1 class="active">Nama Kategori ya</h1>
+		<div class="col-xs-6 col-sm-6">
+			<div class="w3-container">
+				<div class="w3-card-4" style="width:50%">
+					<img src="img_fjords.jpg" alt="Norway" style="width:100%">
+					<div class="w3-container w3-center">
+						<p>The Troll's tongue in Hardanger, Norway</p>
+					</div>
 				</div>
-				<div class="panel-body">
-					<img src="harusnya ini diambil dari db">
+			</div>
+		</div>
+		<div class="col-xs-6 col-sm-6">
+			<div class="w3-container">
+				<div class="w3-card-4" style="width:50%">
+					<img src="img_fjords.jpg" alt="Norway" style="width:100%">
+					<div class="w3-container w3-center">
+						<p>The Troll's tongue in Hardanger, Norway</p>
+					</div>
 				</div>
-				<div class="panel-footer">
-					
+			</div>
+		</div>
+		<div class="col-xs-6 col-sm-6">
+			<div class="w3-container">
+				<div class="w3-card-4" style="width:50%">
+					<img src="img_fjords.jpg" alt="Norway" style="width:100%">
+					<div class="w3-container w3-center">
+						<p>The Troll's tongue in Hardanger, Norway</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-xs-6 col-sm-6">
+			<div class="w3-container">
+				<div class="w3-card-4" style="width:50%">
+					<img src="img_fjords.jpg" alt="Norway" style="width:100%">
+					<div class="w3-container w3-center">
+						<p>The Troll's tongue in Hardanger, Norway</p>
+					</div>
 				</div>
 			</div>
 		</div>
