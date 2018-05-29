4a7d16e2bb4d684562a3969c92a6adde822fd503
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-29 13:51:17 +0700

    radio jenis_kel, no_telp. DONE

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -93,90 +93,101 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="col-md-4 control-label">Password</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                    <div class="col-md-6">
+                        <input id="password" type="password" class="form-control" name="password" required>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                    <div class="col-md-6">
+                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="alamat" class="col-md-4 control-label">Alamat</label>
+
+                    <div class="col-md-6">
+                        <input id="alamat" type="text" class="form-control" name="alamat" value="{{ old('alamat') }}" required autofocus>
+
+                        @if ($errors->has('alamat'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('alamat') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="no_telp" class="col-md-4 control-label">Telephone Number</label>
+
+                    <div class="col-md-6">
+                        <input id="no_telp" type="number" class="form-control" name="no_telp" value="{{ old('no_telp') }}" required autofocus>
+
+                        @if ($errors->has('no_telp'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('no_telp') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="jenis_kelamin" class="col-md-4 control-label">Jenis Kelamin</label>
+
+                    <div>
+                        <div class="radio-inline">
+                            <label>
+                                <input type="radio" name="jenis_kel" id="optionsRadios1" value="Perempuan" checked>Perempuan
+                            </label>
+                        </div>
+                        <div class="radio-inline">
+                            <label>
+                                <input type="radio" name="jenis_kel" id="optionsRadios2" value="Laki-Laki">Laki-Laki
+                            </label>
                         </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                        @if ($errors->has('jenis_kel'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('jenis_kel') }}</strong>
+                            </span>
+                        @endif
+                                        
+                    </div>
+                </div>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="tgl_lahir" class="col-md-4 control-label">Tanggal lahir</label>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="alamat" class="col-md-4 control-label">Alamat</label>
+                    <div class="col-md-6">
+                        <input id="tgl_lahir" type="date" class="form-control" name="tgl_lahir" value="{{ old('tgl_lahir') }}" required autofocus>
 
-                            <div class="col-md-6">
-                                <input id="alamat" type="text" class="form-control" name="alamat" value="{{ old('alamat') }}" required autofocus>
+                         @if ($errors->has('tgl_lahir'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('tgl_lahir') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
 
-                                @if ($errors->has('alamat'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('alamat') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="jenis_kelamin" class="col-md-4 control-label">Jenis Kelamin</label>
-
-                            <div class="col-md-6">
-                                <input id="jenis_kelamin" type="text" class="form-control" name="jenis_kel" value="{{ old('jenis_kel') }}" required autofocus>
-
-                                @if ($errors->has('jenis_kel'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('jenis_kel') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="tgl_lahir" class="col-md-4 control-label">Tanggal lahir</label>
-
-                            <div class="col-md-6">
-                                <input id="tgl_lahir" type="date" class="form-control" name="tgl_lahir" value="{{ old('tgl_lahir') }}" required autofocus>
-
-                                @if ($errors->has('tgl_lahir'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('tgl_lahir') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-sm-10 col-sm-offset-1">
-                                <button type="submit" class="tombol">
-                                    Register
-                                </button>
-                                <br>
-                            </div>
-                        </div>
-                    </form>
-			  <!-- <div class="radio-inline">
-				  <label>
-				    <input type="radio" name="jenis_kel" id="optionsRadios1" value="Perempuan" checked>Perempuan
-				  </label>
-				</div>
-				<div class="radio-inline">
-				  <label>
-				    <input type="radio" name="jenis_kel" id="optionsRadios2" value="Laki-Laki">
-				    Laki-Laki
-				  </label>
-			  </div> -->
+                <div class="form-group">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <button type="submit" class="tombol">Register</button>
+                        <br>
+                    </div>
+                </div>
+            </form>
 		</div>
 	</div>
 @endsection
