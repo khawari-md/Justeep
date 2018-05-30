975a432ba1ff6bd00e2fa174b9aebaa2ff657ec8
aaqmarinaa <almasaqmarina@gmail.com>
2018-05-30 22:39:45 +0700

    oke

--- resources/views/pages/browse.blade.php
+++ resources/views/pages/browse.blade.php
@@ -16,11 +16,9 @@
 			</div>
 	</div>
 	<br>
-  <div data-role="main" class="ui-content">
-    <form>
-      <input id="filterTable-input" data-type="search" placeholder="Search For Customers...">
-    </form>
-  </div>
+
+  <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names..">
+
 	@foreach($data as $data)
 	<div class="container">
 	  <div class="row">
@@ -94,7 +92,11 @@
           </div>
 	        <div class="panel-body"><img class="img-responsive" style="height:150px; width:100%;" alt="Image"></div>
 	        <div class="panel-footer">
-	        	<table class="table table-borderless">
+	        	<table class="table table-borderless" id="myTable">
+                  <tr>
+                    <th>Nama Barang:</th>
+                    <td>{{$data->nama_barang}}</td>
+                  </tr>
                   <tr>
                     <th>Kategori:</th>
                     @if($data->kategori =='1')
@@ -144,4 +146,27 @@
 	    @endforeach
 	  </div>
 	</div>
+
+  <script>
+function myFunction() {
+  // Declare variables 
+  var input, filter, div, tr, td, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+</script>
 @endsection
